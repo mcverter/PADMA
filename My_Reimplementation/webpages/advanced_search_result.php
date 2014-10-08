36b9d966f47f1d80e4cb7b4f6e6e9414f60454e2
mcverter <mitchell.verter@gmail.com>
2014-10-08 11:48:46 -0400

    consolidated search results

--- My_Reimplementation/webpages/advanced_search_result.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once(__DIR__ .   "/../search/AdvancedSearchResultPage.php");
-$p = new AdvancedSearchResultPage();
-$p->display_page();
-
--- My_Reimplementation/webpages/quick_search_result.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once(__DIR__ .   "/../search/QuickSearchResultPage.php");
-$p = new QuickSearchResultPage();
-$p->display_page();
-
